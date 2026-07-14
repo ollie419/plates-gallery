04ea702b6106e5cfef960bf35390e738da8bdd97
Ollie <info@ollieadegboye.com>
2026-07-14 13:21:00 +0100

    connect sanity

---
+++ plates-gallery/src/pages/lib/sanity.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@sanity/client';
+
+export const sanityClient = createClient({
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  apiVersion: import.meta.env.PUBLIC_SANITY_API_VERSION,
+  token: import.meta.env.SANITY_AUTH_TOKEN,
+  useCdn: false,
+}); 
